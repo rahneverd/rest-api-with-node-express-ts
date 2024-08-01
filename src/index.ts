77e31a44fb978e2ff3235c0ad87cc442354bbd87
Muhammad Owais <rahneverd@gmail.com>
2024-08-01 10:04:08 +0300

    done creating server and connection with mongo db

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import compression from 'compression';
+import mongoose from 'mongoose';
+
+dotenv.config();
+const app = express();
+
+app.use(cors({ credentials: true }));
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 8080, () => {
+  console.log(`listening on port ${process.env.PORT || 8080}`);
+});
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.CONNECTIONSTRING || '');
+mongoose.connection.on('error', (err: Error) => {
+  console.log(err);
+});
