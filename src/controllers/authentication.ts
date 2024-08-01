f73a97f9ad1abcdc1055cf4f49e4db5a8e8dfcb8
rahneverd <rahneverd@gmail.com>
2024-08-01 23:11:32 +0500

    login done

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -31,3 +31,45 @@ export const register = async (req: express.Request, res: express.Response) => {
     return res.status(400).json({ error }).end();
   }
 };
+
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ error: 'Missing required fields' }).end();
+    }
+    let user = await getUserByEmail(email).select(
+      '+authentication.salt +authentication.password'
+    );
+    if (!user) {
+      user = await getUserByUsername(email).select(
+        '+authentication.salt +authentication.password'
+      );
+      if (!user) {
+        return res.status(401).json({ error: 'Invalid credentials' }).end();
+      }
+    }
+    const expectedHash = authentication(
+      user?.authentication?.salt || '',
+      password
+    );
+    if (user?.authentication?.password !== expectedHash) {
+      return res.status(403).json({ error: 'Invalid credentials' }).end();
+    }
+    const salt = random();
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+    await user.save();
+    res.cookie('MY-REST-API', user.authentication.sessionToken, {
+      domain: 'localhost',
+      path: '/'
+    });
+    res.status(200).json(user).end();
+    return res.status(200).json({ token: user._id }).end();
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error }).end();
+  }
+};
